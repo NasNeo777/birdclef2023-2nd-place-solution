27708ece6260cb7cd8cb73d49e6a22bf08049bc5
nasneo <2597123782@qq.com>
2026-05-13 16:04:41 +0800

    还原

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
 def crop_or_pad(y, length, is_train=True, start=None):
     if len(y) < length:
         y = np.concatenate([y, np.zeros(length - len(y))])
@@ -18,3 +20,34 @@ def crop_or_pad(y, length, is_train=True, start=None):
         y = y[start:start + length]
 
     return y
+
+
+class SoftAUCLoss(nn.Module):
+    def __init__(self, margin=1.0, pos_weight=1.0, neg_weight=1.0):
+        super().__init__()
+        self.margin = margin
+        self.pos_weight = pos_weight
+        self.neg_weight = neg_weight
+
+def forward(self, preds, labels, sample_weights=None):
+        pos_preds = preds[labels>0.5]
+        neg_preds = preds[labels<0.5]
+        pos_labels = labels[labels>0.5]
+        neg_labels = labels[labels<0.5]
+
+        if len(pos_preds) == 0 or len(neg_preds) == 0:
+            return torch.tensor(0.0, device=preds.device)
+
+        pos_weights = torch.ones_like(pos_preds) * self.pos_weight * (pos_labels-0.5)
+        neg_weights = torch.ones_like(neg_preds) * self.neg_weight * (0.5-neg_labels)
+        if sample_weights is not None:
+            sample_weights = torch.stack([sample_weights]*labels.shape[1], dim=1)
+            pos_weights = pos_weights * sample_weights
+            neg_weights = neg_weights * sample_weights
+
+        diff = pos_preds.unsqueeze(1) - neg_preds.unsqueeze(0)  # [N_pos, N_neg]
+        loss_matrix = torch.log(1 + torch.exp(-diff * self.margin))  # [N_pos, N_neg]
+
+        weighted_loss = loss_matrix * pos_weights.unsqueeze(1) * neg_weights.unsqueeze(0)
+
+        return weighted_loss.mean()
