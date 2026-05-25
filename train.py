5ea5a59a74523562404253e6c395cc382e9ecb48
nasneo <2597123782@qq.com>
2026-05-25 10:30:22 +0800

    LLRD

--- train.py
+++ train.py
@@ -196,7 +196,7 @@ def main():
     )
 
     logger = WandbLogger(
-        project=f'BirdClef-SoftLoss-LLRD-{cfg.dataset_version}',
+        project=f'BirdClef-SoftLoss-LLRD_all_model-{cfg.dataset_version}',
         name=f'{model_name}_{stage}',
         settings=wandb.Settings(quiet=True, console="off"),
     )
